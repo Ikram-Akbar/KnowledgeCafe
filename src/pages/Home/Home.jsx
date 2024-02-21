8b494855c1cab8372f1badb93504a33520ef2bb4
ikram akbar <ikramakbar71@gmail.com>
2024-02-21 11:31:05 +0600

    project set up ready with tailwing

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-blue-400 text-4xl font-bold underline">
+                Hello world!
+            </h1>
+
+        </div>
+    );
+};
+
+export default Home;
